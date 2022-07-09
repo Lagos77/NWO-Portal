449f9d45ff233c71666bcd1a37790432865aeac7
Lagos77 <social_a@live.com>
2022-07-09 08:53:11 +0200

    added

--- src/Routes/Information/Information.jsx
+++ src/Routes/Information/Information.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Information.css"
 
+import usa from './usa.png';
+import swe from './swe.png';
+import nato from './nato.png';
+
 const Information = () => {
 
     return (
-        <div>
-            <p>This site will contain information</p>
+        <div className="info-container">
+
+            <div className="container">
+                <div className="img-cont">
+                    <img src={usa} alt="" />
+                </div>
+                <section>
+                    <h3>United States</h3>
+                </section>
+            </div>
+
+            <div className="container">
+                <div className="img-cont">
+                    <img src={swe} alt="" />
+                </div>
+                <section>
+                    <h3>Sweden</h3>
+                </section>
+            </div>
+
+            <div className="container">
+                <div className="img-cont">
+                    <img src={nato} alt="" />
+                </div>
+                <section>
+                    <h3>NATO</h3>
+                </section>
+            </div>
+
         </div>
     )
 }
