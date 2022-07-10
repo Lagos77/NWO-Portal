06c9979c50fa399a1c801d95d034bd1815203768
Lagos77 <social_a@live.com>
2022-07-10 02:52:39 +0200

    added

--- src/Routes/Information/Information.jsx
+++ src/Routes/Information/Information.jsx
@@ -1,42 +1,28 @@
 import React from "react";
 import "./Information.css"
 
-import usa from './usa.png';
-import swe from './swe.png';
-import nato from './nato.png';
+import marina from './marina.jpeg'
 
 const Information = () => {
 
     return (
-        <div className="info-container">
-
-            <div className="container">
-                <div className="img-cont">
-                    <img src={usa} alt="" />
+        <div className="info-body">
+            <div className="card">
+                <div className="image-content">
+                    <span className="overlay"></span>
+                    <div className="card-image">
+                        <img src={marina} alt="" className="card-img" />
+                    </div>
                 </div>
-                <section>
-                    <h3>United States</h3>
-                </section>
-            </div>
-
-            <div className="container">
-                <div className="img-cont">
-                    <img src={swe} alt="" />
+                <div className="card-content">
+                    <h2 className="name">Marina Abramović</h2>
+                    <h3 className="dedication">Body Art/Shock Art</h3>
+                    <p className="description">
+                        testing testing testing and more testing
+                    </p>
+                    <button className="button">More Info</button>
                 </div>
-                <section>
-                    <h3>Sweden</h3>
-                </section>
             </div>
-
-            <div className="container">
-                <div className="img-cont">
-                    <img src={nato} alt="" />
-                </div>
-                <section>
-                    <h3>NATO</h3>
-                </section>
-            </div>
-
         </div>
     )
 }
