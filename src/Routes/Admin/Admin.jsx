cec27aef6c09f750c84abc7a5e3c4b7708265f34
Lagos77 <social_a@live.com>
2022-07-11 03:06:34 +0200

    fixing

--- src/Routes/Admin/Admin.jsx
+++ src/Routes/Admin/Admin.jsx
@@ -1,81 +1,35 @@
-import React, { useState } from 'react'
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../Firebase/firebase-config"
+import React from 'react'
+import "./firebase-config"
 import './Admin.css'
 
 const Admin = () => {
 
-    const [registerEmail, setRegisterEmail] = useState("");
-    const [registerPassword, setRegisterPassword] = useState("");
-    const [loginEmail, setLoginEmail] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
 
-    const register = async () => {
-        try {
-        const user = await createUserWithEmailAndPassword(
-            auth, 
-            registerEmail, 
-            registerPassword
-            );
-        console.log(user);
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
-
-    const login = async () => {
-
-    }
-
-    const logout =  async () => {
-
-    }
 
     return (
         <div className='admin-container'>
             <div>
-                <h3> Register User </h3>
-                <input
-                    type="text"      
-                    placeholder='Email...' 
-                    onChange={(event) => {
-                    setRegisterEmail(event.target.value);
-                }} 
-                />
-                <input
-                    type="text" 
-                    placeholder='Password...' 
-                    onChange={(event) => {
-                    setRegisterPassword(event.target.value);
-                }} 
-                />
+                <h3>Register User</h3>
 
-                <button> Create User </button>
+                <input type="text" placeholder='Email...' />
+
+                <input type="text" placeholder='Password...'/>
+
+                <button>Register</button>
             </div>
-
             <div>
-                <h3> Login </h3>
-                <input 
-                    type="text" 
-                    placeholder='Email...' 
-                    onChange={(event) => {
-                    setLoginEmail(event.target.value);
-                }} 
-                />
-                <input 
-                    type="text" 
-                    placeholder='Password...' 
-                    onChange={(event) => {
-                    setLoginPassword(event.target.value);
-                }} 
-                />
+                <h3>Login</h3>
 
-                <button> Login </button>
+                <input type="text" placeholder='Email...' />
+
+                <input type="text" placeholder='Password...'/>
+
+                <button>Login</button>
+            </div>
+            <div>
+                <p>Current user is logged in</p>
             </div>
 
-            <h4> User logged in: </h4>
-
-            <button> Sign Out </button>
         </div>
     )
 }
